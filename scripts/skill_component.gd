984536721e006188ddcd15e316210742d5c7123d
Yohannes Laksana <yoholaksana@gmail.com>
2026-07-02 07:44:41 -0500

    made skill search in skillcomponent to separate basic attacks from special attacks

--- scripts/skill_component.gd
+++ scripts/skill_component.gd
@@ -2,7 +2,7 @@ class_name SkillComponent extends Node
 
 @export var body: Player
 @export var skill_timer: Timer
-
+@export var skills: Array[AttackData]
 const FIREBALL = preload("res://scenes/fireball.tscn")
 
 var wants_special_attack: bool = false
@@ -22,17 +22,15 @@ func tick(delta: float) -> void:
 			return
 		if wants_special_attack:
 			#print("fireball!")
-			body.attack_component.set_curr_atk("fireball")
 			fireball()
 		
 
 func fireball() -> void:
-	skill = body.hitbox_component.curr_atk
+	skill = skills.filter(func(atk): return atk.animation_name == "fireball").front()
 	spawn_delay = skill.spawn_delay
 	await get_tree().create_timer(spawn_delay).timeout
 	var fireball_instance = FIREBALL.instantiate()
 	get_tree().root.add_child(fireball_instance)
 	fireball_instance.global_position = body.global_position
 	fireball_instance.rotation = 0.0 if body.animated_sprite.flip_h else PI
-	fireball_instance.hitbox_component.curr_atk = body.hitbox_component.curr_atk
-	
+	fireball_instance.hitbox_component.curr_atk = skill
