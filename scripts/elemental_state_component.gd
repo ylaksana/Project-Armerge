80b59000e8bdc078993387ae45fe7482f573fa7c
Yohannes Laksana <yoholaksana@gmail.com>
2026-07-13 21:48:41 -0500

    reset to current state

--- scripts/elemental_state_component.gd
+++ scripts/elemental_state_component.gd
@@ -18,3 +18,23 @@ var curr_elemental_state: ElementState = ElementState.NONE
 func _ready() -> void:
 	hitbox.hit.connect(_on_hit)
 	
+func _on_hit() -> void:
+	# if cooldown timer stopped:
+	if not elemental_cooldown_timer.is_stopped():
+		# apply the element:
+		if curr_elemental_state == ElementState.NONE:
+			apply_element()
+		else:
+			reaction_component.elemental_reaction()
+			
+	
+	# if cooldown timer is still active:
+	
+func apply_element() -> void:
+	if hitbox.curr_atk and hitbox.curr_atk.has_element:
+		if hitbox.curr_atk.element_type == AttackData.ElementType.FIRE:
+			curr_elemental_state = ElementState.FIRE
+		elif hitbox.curr_atk.element_type == AttackData.ElementType.ICE:
+			curr_elemental_state = ElementState.ICE
+		elif hitbox.curr_atk.element_type == AttackData.ElementType.ELECTRIC:
+			curr_elemental_state = ElementState.ELECTRIC
